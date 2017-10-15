786818ae2747b4ce99d86dff917ed7fef69e537f
Mladen Sijan <mladen.sijan@live.com>
2017-10-15 19:53:44 +0200

    Dodata skripta za validaciju (nije implementirana), uredjen css za mail formu

---
+++ js/mailValidacija.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.mail-forma').validate( {
+    rules: {
+      //inputname: { pravila }
+    },
+    messages: {
+      //inputname: { poruke za pravila }
+    }
+  });
+});
